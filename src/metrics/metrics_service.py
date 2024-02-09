f60c17621db31ac1895b7513c0f39d91199d57cf
Vlad15lav <vladislav.viryasov@gmail.com>
2024-02-09 21:10:19 +0300

    update metrics service

--- src/metrics/metrics_service.py
+++ src/metrics/metrics_service.py
@@ -1,7 +1,9 @@
+import numpy as np
 import pandas as pd
 
 from pydantic import BaseModel
-# from datetime import datetime
+from datetime import datetime
+from datetime import timedelta
 
 
 class DataService:
@@ -207,20 +209,82 @@ class MetricsService:
 
         users_revenue = users_revenue.groupby('user_id')['price'] \
             .sum().reset_index()
+
         return users_revenue.rename(columns={'price': 'metric'})
+
+    @staticmethod
+    def _calculate_theta_cuped(metric, metric_cov):
+        """Считаем theta для CUPED
+
+        :param metric (datetime): дата начала периода (включая границу)
+        :param metric_cov (datetime): дата окончания периода
+            (не включая границу)
+
+        :return theta (float): theta значение CUPED
+        """
+        cov_matrix = np.cov((metric, metric_cov))[0][1]
+        variance = metric_cov.var()
+        return cov_matrix / variance
+
+    def _calculate_revenue_cuped(self,
+                                 begin_date,
+                                 end_date,
+                                 user_ids=None,
+                                 days=7):
+        """Считаем метрику CUPED
+
+        :param begin_date (datetime): дата начала периода (включая границу)
+        :param end_date (datetime): дата окончания периода (не включая границу)
+        :param user_ids (list[str], None): список пользователей.
+            Если None, то вычисляет метрику для всех пользователей.
+        :param days (int): количество дней предэксперемента
+
+        :return df: columns=['user_id', 'metric']
+        """
+        X_metric = self._calculate_revenue_web(
+            begin_date=begin_date - timedelta(days=days),
+            end_date=begin_date,
+            user_ids=user_ids).rename(columns={'metric': 'cov'})
+        Y_metric = self._calculate_revenue_web(
+            begin_date=begin_date,
+            end_date=end_date,
+            user_ids=user_ids)
+        if user_ids:
+            df = pd.DataFrame({'user_id': user_ids})
+        else:
+            df = pd.concat((X_metric[['user_id']],
+                            Y_metric[['user_id']])).drop_duplicates()
+
+        df = df.merge(right=X_metric, how='left', on='user_id')
+        df = df.merge(right=Y_metric, how='left', on='user_id').fillna(0)
+
+        X_metric = df['cov'].values
+        Y_metric = df['metric'].values
+
+        theta = self._calculate_theta_cuped(Y_metric, X_metric)
+        Y_cuped = Y_metric - theta * (X_metric - np.mean(X_metric))
+        df['metric'] = Y_cuped
+
+        return df[['user_id', 'metric']]
 
     def calculate_metric(self,
                          metric_name,
-                         begin_date,
-                         end_date,
+                         begin_date, end_date,
+                         cuped='off',
                          user_ids=None):
-        """Считает значения для вычисления метрик.
+        """Считает значения метрики.
 
         :param metric_name (str): название метрики
         :param begin_date (datetime): дата начала периода (включая границу)
         :param end_date (datetime): дата окончания периода (не включая границу)
+        :param cuped (str): применение CUPED.
+            ['off', 'on (previous week revenue)']
+            'off' - не применять CUPED
+            'on (previous week revenue)' - применяем CUPED,
+                в качестве ковариаты используем выручку за прошлые 7 дней
         :param user_ids (list[str], None): список пользователей.
-            Если None, то вычисляет значения для всех пользователей.
+            Если None, то вычисляет метрику для всех пользователей.
+
         :return df: columns=['user_id', 'metric']
         """
         if metric_name == 'response time':
@@ -228,9 +292,21 @@ class MetricsService:
                                                  end_date,
                                                  user_ids)
         elif metric_name == 'revenue (web)':
-            return self._calculate_revenue_web(begin_date, end_date, user_ids)
+            if cuped == 'off':
+                return self._calculate_revenue_web(begin_date,
+                                                   end_date,
+                                                   user_ids)
+            elif cuped == 'on (previous week revenue)':
+                return self._calculate_revenue_cuped(begin_date,
+                                                     end_date,
+                                                     user_ids,
+                                                     days=7)
+            else:
+                raise ValueError('Wrong cuped')
         elif metric_name == 'revenue (all)':
-            return self._calculate_revenue_all(begin_date, end_date, user_ids)
+            return self._calculate_revenue_all(begin_date,
+                                               end_date,
+                                               user_ids)
         else:
             raise ValueError('Wrong metric name')
 
@@ -261,7 +337,8 @@ class MetricsService:
         return metrics
 
 
-def _chech_df(df, df_ideal, sort_by, reindex=False, set_dtypes=False):
+def _chech_df(df, df_ideal, sort_by, reindex=False,
+              set_dtypes=False, decimal=None):
     assert isinstance(df, pd.DataFrame), 'Функция вернула не pd.DataFrame.'
     assert len(df) == len(df_ideal), 'Неверное количество строк.'
     assert len(df.T) == len(df_ideal.T), 'Неверное количество столбцов.'
@@ -276,53 +353,58 @@ def _chech_df(df, df_ideal, sort_by, reindex=False, set_dtypes=False):
     if set_dtypes:
         for column, dtype in df_ideal.dtypes.to_dict().items():
             df[column] = df[column].astype(dtype)
-    assert df_ideal.equals(df), \
-        'Итоговый датафрейм не совпадает с верным результатом.'
+    if decimal:
+        ideal_values = df_ideal.astype(float).values
+        values = df.astype(float).values
+        np.testing.assert_almost_equal(ideal_values, values, decimal=decimal)
+    else:
+        assert df_ideal.equals(df), \
+            'Итоговый датафрейм не совпадает с верным результатом.'
 
 
 if __name__ == '__main__':
     # Test for revenue metric
-    # df_sales = pd.DataFrame({
-    #     'sale_id': [1, 2, 3],
-    #     'date': [datetime(2022, 3, day, 11) for day in range(11, 14)],
-    #     'price': [1100, 900, 1500],
-    #     'user_id': ['1', '2', '1'],
-    # })
-    # df_web_logs = pd.DataFrame({
-    #     'date': [datetime(2022, 3, day, 11) for day in range(10, 14)],
-    #     'load_time': [80.8, 90.1, 15.8, 19.7],
-    #     'user_id': ['3', '1', '2', '1'],
-    # })
-    # begin_date = datetime(2022, 3, 11, 9)
-    # end_date = datetime(2022, 4, 11, 9)
+    df_sales = pd.DataFrame({
+        'sale_id': [1, 2, 3],
+        'date': [datetime(2022, 3, day, 11) for day in range(11, 14)],
+        'price': [1100, 900, 1500],
+        'user_id': ['1', '2', '1'],
+    })
+    df_web_logs = pd.DataFrame({
+        'date': [datetime(2022, 3, day, 11) for day in range(10, 14)],
+        'load_time': [80.8, 90.1, 15.8, 19.7],
+        'user_id': ['3', '1', '2', '1'],
+    })
+    begin_date = datetime(2022, 3, 11, 9)
+    end_date = datetime(2022, 4, 11, 9)
 
-    # ideal_response_time = pd.DataFrame({'user_id': ['1', '2', '1'],
-    #                                     'metric': [90.1, 15.8, 19.7]})
-    # ideal_revenue_web = pd.DataFrame({'user_id': ['1', '2'],
-    #                                   'metric': [2600., 900.]})
-    # ideal_revenue_all = pd.DataFrame({'user_id': ['1', '2', '3'],
-    #                                   'metric': [2600., 900., 0.]})
+    ideal_response_time = pd.DataFrame({'user_id': ['1', '2', '1'],
+                                        'metric': [90.1, 15.8, 19.7]})
+    ideal_revenue_web = pd.DataFrame({'user_id': ['1', '2'],
+                                      'metric': [2600., 900.]})
+    ideal_revenue_all = pd.DataFrame({'user_id': ['1', '2', '3'],
+                                      'metric': [2600., 900., 0.]})
 
-    # data_service = DataService({'sales': df_sales, 'web-logs': df_web_logs})
-    # metrics_service = MetricsService(data_service)
+    data_service = DataService({'sales': df_sales, 'web-logs': df_web_logs})
+    metrics_service = MetricsService(data_service)
 
-    # df_response_time = metrics_service.calculate_metric('response time',
-    #                                                     begin_date,
-    #                                                     end_date)
-    # df_revenue_web = metrics_service.calculate_metric('revenue (web)',
-    #                                                   begin_date,
-    #                                                   end_date)
-    # df_revenue_all = metrics_service.calculate_metric('revenue (all)',
-    #                                                   begin_date,
-    #                                                   end_date)
+    df_response_time = metrics_service.calculate_metric('response time',
+                                                        begin_date,
+                                                        end_date)
+    df_revenue_web = metrics_service.calculate_metric('revenue (web)',
+                                                      begin_date,
+                                                      end_date)
+    df_revenue_all = metrics_service.calculate_metric('revenue (all)',
+                                                      begin_date,
+                                                      end_date)
 
-    # _chech_df(df_response_time, ideal_response_time, ['user_id', 'metric'],
-    #           True, True)
-    # _chech_df(df_revenue_web, ideal_revenue_web, ['user_id', 'metric'],
-    #           True, True)
-    # _chech_df(df_revenue_all, ideal_revenue_all, ['user_id', 'metric'],
-    #           True, True)
-    # print('simple test passed')
+    _chech_df(df_response_time, ideal_response_time, ['user_id', 'metric'],
+              True, True)
+    _chech_df(df_revenue_web, ideal_revenue_web, ['user_id', 'metric'],
+              True, True)
+    _chech_df(df_revenue_all, ideal_revenue_all, ['user_id', 'metric'],
+              True, True)
+    print('simple test passed')
 
     # Test for remove outlier
     metrics = pd.DataFrame({
@@ -344,4 +426,32 @@ if __name__ == '__main__':
     processed_metrics = metrics_service.process_outliers(metrics, design)
     _chech_df(processed_metrics, ideal_processed_metrics,
               ['user_id', 'metric'], True, True)
+    print('simple test passed')
+
+    # Test for CUPED
+    df_sales = pd.DataFrame({
+        'sale_id': [1, 2, 3, 4, 5],
+        'date': [datetime(2022, 3, day, 11) for day in range(10, 15)],
+        'price': [1100, 1500, 2000, 2500, 3000],
+        'user_id': ['1', '2', '1', '2', '3'],
+    })
+    df_web_logs = pd.DataFrame({
+        'date': [datetime(2022, 3, day, 11) for day in range(10, 15)],
+        'user_id': ['1', '2', '1', '2', '3'],
+    })
+    begin_date = datetime(2022, 3, 12, 0)
+    end_date = datetime(2022, 3, 19, 0)
+
+    ideal_metrics = pd.DataFrame({
+        'user_id': ['1', '2', '3'],
+        'metric': [2159.5303, 2933.0110, 2407.45856],
+    })
+
+    data_service = DataService({'sales': df_sales, 'web-logs': df_web_logs})
+    metrics_service = MetricsService(data_service)
+    metrics = metrics_service.calculate_metric(
+        'revenue (web)', begin_date, end_date, 'on (previous week revenue)'
+    )
+    _chech_df(metrics, ideal_metrics, ['user_id', 'metric'], True,
+              True, decimal=1)
     print('simple test passed')
